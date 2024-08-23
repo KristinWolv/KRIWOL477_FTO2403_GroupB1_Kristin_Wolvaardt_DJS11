9e8745aa5318693693924366331f6a23ece5c112
KristinWolv <162977890+KristinWolv@users.noreply.github.com>
2024-08-23 15:15:34 +0200

    Change Switch to Routes

--- EchoPod/src/App.jsx
+++ EchoPod/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import useStore from './store';
 import Home from './components/Home';
 import ShowDetails from './components/ShowDetails';
 import Favourites from './components/Favourites';
 import AudioPlayer from './components/AudioPlayer';
+
 
 function App() {
   const fetchShows = useStore((state) => state.fetchShows);
@@ -16,11 +17,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
+        <Routes>
           <Route path="/" exact component={Home} />
           <Route path="/show/:id" component={ShowDetails} />
           <Route path="/favourites" component={Favourites} />
-        </Switch>
+        </Routes>
         <AudioPlayer />
       </div>
     </Router>
